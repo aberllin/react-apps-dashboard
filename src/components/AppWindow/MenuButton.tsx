9cb1de722ba3a949aaaf46d4b64ee248ea92ec68
aberllin <ns.kozluk@gmail.com>
2021-01-20 11:14:04 +0300

    add menubutton

--- src/components/AppWindow/MenuButton.tsx
+++ src/components/AppWindow/MenuButton.tsx
@@ -1,10 +1,10 @@
-import {HiOutlineDotsCircleHorizontal} from 'react-icons/hi'
-
-
+import { HiOutlineDotsCircleHorizontal } from 'react-icons/hi'
+import React from 'react'
 
 export const MenuButton = () => {
+  return (
     <div>
-        <HiOutlineDotsCircleHorizontal />
+      <HiOutlineDotsCircleHorizontal />
     </div>
-   
+  )
 }
